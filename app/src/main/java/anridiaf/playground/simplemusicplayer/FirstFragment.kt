1e84029e6da291fcb417a5516e36708b4f6e65d7
Afriandi <emailafriandi@gmail.com>
2024-07-13 02:57:43 +0700

    update (main) : Implement adapter to recycler view

--- app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
@@ -3,14 +3,18 @@ package anridiaf.playground.simplemusicplayer
 import android.content.ContentResolver
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.OptIn
 import androidx.fragment.app.Fragment
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaController
 import androidx.media3.session.MediaSession
+import androidx.recyclerview.widget.LinearLayoutManager
 import anridiaf.playground.simplemusicplayer.databinding.FragmentFirstBinding
 
 /**
@@ -39,11 +43,17 @@ class FirstFragment : Fragment() {
         return binding.root
     }
 
+    @OptIn(UnstableApi::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.controller.visibility = View.INVISIBLE
         binding.controller.player = exoPlayer
-        exoPlayer.setMediaItem(MediaItem.fromUri(uri))
-        exoPlayer.prepare()
+        binding.playlist.layoutManager = LinearLayoutManager(requireContext())
+        binding.playlist.adapter = SongItemAdapter(dummySongs){
+            exoPlayer.setMediaItem(MediaItem.fromUri(uri))
+            exoPlayer.prepare()
+            binding.controller.visibility = View.VISIBLE
+        }
     }
 
     override fun onDestroyView() {
@@ -51,3 +61,8 @@ class FirstFragment : Fragment() {
         _binding = null
     }
 }
+
+val dummySongs = listOf(
+    Song(title = "How to Save a Life", artist = "The Fray", album = "Mock Album"),
+    Song(title = "Thousand miles", artist = "Vanessa Carlton", album = "Mock Album"),
+)
