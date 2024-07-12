3418a456837374a386691608c2b9cbcb21c8f584
Afriandi <emailafriandi@gmail.com>
2024-07-12 23:21:22 +0700

    update (main) : Add sample implementation and add Pedro

--- app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
@@ -1,10 +1,16 @@
 package anridiaf.playground.simplemusicplayer
 
+import android.content.ContentResolver
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.MediaSession
 import anridiaf.playground.simplemusicplayer.databinding.FragmentFirstBinding
 
 /**
@@ -14,6 +20,15 @@ class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
+    private val exoPlayer : ExoPlayer by lazy { ExoPlayer.Builder(requireContext()).build() }
+    private val session : MediaSession by lazy { MediaSession.Builder(requireContext(), exoPlayer).build() }
+    private val controller by lazy {
+        MediaController.Builder(requireContext(), session.token).buildAsync()
+    }
+    private val uri: Uri = Uri.Builder()
+        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        .path(R.raw.pedro.toString())
+        .build()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +41,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.controller.player = exoPlayer
+        exoPlayer.setMediaItem(MediaItem.fromUri(uri))
+        exoPlayer.prepare()
     }
 
     override fun onDestroyView() {
