9fc25652253599410b9792d29916eb0a11168435
Afriandi <emailafriandi@gmail.com>
2024-07-12 23:04:55 +0700

    update (main) : Remove unused UI

--- app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
@@ -1,11 +1,10 @@
 package anridiaf.playground.simplemusicplayer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import anridiaf.playground.simplemusicplayer.databinding.FragmentFirstBinding
 
 /**
@@ -27,10 +26,6 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
