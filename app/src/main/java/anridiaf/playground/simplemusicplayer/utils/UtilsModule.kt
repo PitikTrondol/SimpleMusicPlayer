8aaef4bb1a70e6e0385b2f0764a65d23da189b92
Afriandi <emailafriandi@gmail.com>
2024-07-13 15:41:32 +0700

    update (main) : Add Ktor client to injection

--- app/src/main/java/anridiaf/playground/simplemusicplayer/utils/UtilsModule.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/utils/UtilsModule.kt
@@ -1,2 +1,9 @@
 package anridiaf.playground.simplemusicplayer.utils
 
+import io.ktor.client.HttpClient
+import org.koin.dsl.module
+
+val utilModule = module {
+
+    factory<HttpClient> { KtorHttpClient.createClient() }
+}
