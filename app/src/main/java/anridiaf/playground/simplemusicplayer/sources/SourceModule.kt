bcd386c15b4fc01329428e1be977a2389bff1004
Afriandi <emailafriandi@gmail.com>
2024-07-13 16:55:00 +0700

    add (main) : Add dispatcher to manager implementation

--- app/src/main/java/anridiaf/playground/simplemusicplayer/sources/SourceModule.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/sources/SourceModule.kt
@@ -2,13 +2,17 @@ package anridiaf.playground.simplemusicplayer.sources
 
 import anridiaf.playground.simplemusicplayer.sources.songdata.SongDataManager
 import anridiaf.playground.simplemusicplayer.sources.songdata.SongDataManagerImpl
+import anridiaf.playground.simplemusicplayer.utils.ioDispatcher
 import io.ktor.client.HttpClient
+import kotlinx.coroutines.CoroutineDispatcher
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataSourceModule = module {
     factory<SongDataManager> {
         SongDataManagerImpl(
-            httpClient = get<HttpClient>()
+            httpClient = get<HttpClient>(),
+            dispatcher = get<CoroutineDispatcher>(qualifier = named(ioDispatcher))
         )
     }
 }
