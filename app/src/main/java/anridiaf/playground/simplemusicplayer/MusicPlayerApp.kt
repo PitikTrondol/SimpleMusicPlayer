1a42225eab18af1bec00edc64838da3746d541f3
Afriandi <emailafriandi@gmail.com>
2024-07-13 15:43:39 +0700

    update (main) : Implement Koin

--- app/src/main/java/anridiaf/playground/simplemusicplayer/MusicPlayerApp.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/MusicPlayerApp.kt
@@ -1,9 +1,25 @@
 package anridiaf.playground.simplemusicplayer
 
 import android.app.Application
+import anridiaf.playground.simplemusicplayer.sources.dataSourceModule
+import anridiaf.playground.simplemusicplayer.utils.utilModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MusicPlayerApp: Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(androidContext = applicationContext)
+            androidLogger(Level.INFO)
+            modules(
+                listOf(
+                    utilModule,
+                    dataSourceModule
+                )
+            )
+        }
     }
 }
