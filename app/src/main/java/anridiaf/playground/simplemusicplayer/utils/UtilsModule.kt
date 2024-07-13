b85975986f22b39556ab1c26b10010644a79209b
Afriandi <emailafriandi@gmail.com>
2024-07-13 16:55:18 +0700

    add (main) : Add dispatcher injection

--- app/src/main/java/anridiaf/playground/simplemusicplayer/utils/UtilsModule.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/utils/UtilsModule.kt
@@ -1,9 +1,26 @@
 package anridiaf.playground.simplemusicplayer.utils
 
 import io.ktor.client.HttpClient
+import kotlinx.coroutines.Dispatchers
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val utilModule = module {
 
     factory<HttpClient> { KtorHttpClient.createClient() }
+    single(named(ioDispatcher)) {
+        Dispatchers.IO
+    }
+
+    single(named(defaultDispatcher)) {
+        Dispatchers.Default
+    }
+
+    single(named(mainDispatcher)) {
+        Dispatchers.Main
+    }
 }
+
+const val ioDispatcher = "IODispatcher"
+const val mainDispatcher = "MainDispatcher"
+const val defaultDispatcher = "DefaultDispatcher"
