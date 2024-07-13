6ea37068797aa954995711c4f0df5270fa2908a1
Afriandi <emailafriandi@gmail.com>
2024-07-13 16:53:25 +0700

    add (main) : Add dispatcher to ViewModel

--- app/src/main/java/anridiaf/playground/simplemusicplayer/presenter/PresenterModule.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/presenter/PresenterModule.kt
@@ -1,13 +1,17 @@
 package anridiaf.playground.simplemusicplayer.presenter
 
 import anridiaf.playground.simplemusicplayer.sources.songdata.SongDataManager
+import anridiaf.playground.simplemusicplayer.utils.defaultDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val presenterModule = module {
     viewModel<SongManagerViewModel> {
         SongManagerViewModel(
-            songDataManager = get<SongDataManager>()
+            songDataManager = get<SongDataManager>(),
+            defaultDispatcher = get<CoroutineDispatcher>(qualifier = named(defaultDispatcher))
         )
     }
 }
