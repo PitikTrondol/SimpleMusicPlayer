c61a642f457699ea410aa01425f97c7bc4852f2c
Afriandi <emailafriandi@gmail.com>
2024-07-13 15:42:03 +0700

    update (main) : Handler error and success

--- app/src/main/java/anridiaf/playground/simplemusicplayer/utils/ResultWrapper.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/utils/ResultWrapper.kt
@@ -1,4 +1,17 @@
 package anridiaf.playground.simplemusicplayer.utils
 
-class ResultWrapper {
+sealed class ResultWrapper<out T : Any> {
+
+    data class Success<T : Any>(
+        val data: T
+    ) : ResultWrapper<T>()
+
+    data class Error(
+        val statusResponse: String = ""
+    ) : ResultWrapper<Nothing>()
+
+    inline fun <V> fold(failure: (String) -> V, success: (T) -> V): V = when (this) {
+        is Error -> failure(statusResponse)
+        is Success -> success(data)
+    }
 }
