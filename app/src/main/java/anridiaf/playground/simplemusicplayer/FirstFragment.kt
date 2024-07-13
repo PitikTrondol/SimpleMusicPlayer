98f2afeb6fd44d2af2df070b914f29f4c2d273d8
Afriandi <emailafriandi@gmail.com>
2024-07-13 13:47:19 +0700

    update (main) : Initial implementation of online song source

--- app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/FirstFragment.kt
@@ -9,33 +9,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.OptIn
 import androidx.fragment.app.Fragment
-import androidx.media3.common.C
 import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
 import androidx.media3.common.Player
-import androidx.media3.common.Tracks
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.session.MediaController
-import androidx.media3.session.MediaSession
 import androidx.recyclerview.widget.LinearLayoutManager
 import anridiaf.playground.simplemusicplayer.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+@OptIn(UnstableApi::class)
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
-    private val exoPlayer : ExoPlayer by lazy { ExoPlayer.Builder(requireContext()).build() }
-    private val session : MediaSession by lazy { MediaSession.Builder(requireContext(), exoPlayer).build() }
-    private val controller by lazy {
-        MediaController.Builder(requireContext(), session.token).buildAsync()
+    private val exoPlayer: ExoPlayer by lazy { ExoPlayer.Builder(requireContext()).build() }
+
+    private val onlineTracks = songUrl.map {
+        MediaItem.Builder()
+            .setUri(it)
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .build()
+            )
+            .build()
     }
-    private val uri: Uri = Uri.Builder()
-        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-        .path(R.raw.pedro.toString())
-        .build()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,43 +46,28 @@ class FirstFragment : Fragment() {
         return binding.root
     }
 
-    @OptIn(UnstableApi::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val itemList = dummySongs.map {
-            MediaItem.fromUri(uri)
-        }
-        val adapter = SongItemAdapter(dummySongs){index->
-            exoPlayer.setMediaItems(
-                itemList,
-                index,
-                C.TIME_UNSET
-            )
-            exoPlayer.prepare()
-            binding.controller.visibility = View.VISIBLE
-        }
+        val adapter = SongItemAdapter(onlineTracks) { index ->
 
-        binding.controller.visibility = View.INVISIBLE
-        binding.controller.player = exoPlayer
+        }
         binding.playlist.layoutManager = LinearLayoutManager(requireContext())
         binding.playlist.adapter = adapter
 
+        binding.controller.player = exoPlayer
+        exoPlayer.addMediaItems(onlineTracks)
+        exoPlayer.prepare()
         exoPlayer.addListener(
-            object : Player.Listener{
-                override fun onIsPlayingChanged(isPlaying: Boolean) {
-                    Log.e("AFRI", "onIsPlayingChanged: ")
-                    super.onIsPlayingChanged(isPlaying)
-                    adapter.startPlaying(
-                        exoPlayer.currentMediaItemIndex,
-                        dummySongs[exoPlayer.currentMediaItemIndex].copy(
-                            isPlaying = isPlaying
-                        )
-                    )
-                }
-
+            object : Player.Listener {
                 override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
                     super.onMediaItemTransition(mediaItem, reason)
-                    mediaItem
+                    Log.e("AFRI", "onMediaItemTransition: ${mediaItem?.mediaMetadata?.artworkUri}")
+                    Log.e("AFRI", "onMediaItemTransition: ${mediaItem?.mediaMetadata?.artworkDataType}")
+                }
+
+                override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+                    Log.e("AFRI", "onMediaMetadataChanged: ${mediaMetadata.artist}")
+                    super.onMediaMetadataChanged(mediaMetadata)
                 }
             }
         )
@@ -94,7 +79,14 @@ class FirstFragment : Fragment() {
     }
 }
 
-val dummySongs = listOf(
-    Song(title = "How to Save a Life", artist = "The Fray", album = "Mock Album"),
-    Song(title = "Thousand miles", artist = "Vanessa Carlton", album = "Mock Album"),
+val songUrl = listOf(
+    "https://cdn.pixabay.com/download/audio/2024/01/25/audio_599143a433.mp3?filename=beyond-horizons-187963.mp3",
+    "https://cdn.pixabay.com/download/audio/2023/12/14/audio_45a981149b.mp3?filename=fvcksin-181360.mp3",
+    "https://cdn.pixabay.com/download/audio/2024/05/10/audio_ba4c81d07e.mp3?filename=one-step-closer-207958.mp3",
+    "https://cdn.pixabay.com/download/audio/2024/05/08/audio_373e05c162.mp3?filename=soaring-heights-207507.mp3",
+    "https://cdn.pixabay.com/download/audio/2024/04/14/audio_52070f6f9b.mp3?filename=happy-202230.mp3",
+    "https://cdn.pixabay.com/download/audio/2021/12/01/audio_e2a795a65c.mp3?filename=words-unsaid-11540.mp3",
+    "https://cdn.pixabay.com/download/audio/2024/06/26/audio_69c4bb112f.mp3?filename=epic-piano-music-deception-point-219963.mp3",
+    "https://cdn.pixabay.com/download/audio/2023/09/17/audio_6b45d4469c.mp3?filename=ambition-multiple-asian-music-instruments-166931.mp3",
+    "https://cdn.pixabay.com/download/audio/2021/08/27/audio_759906caf1.mp3?filename=uplifting-piano-background-music-for-videos-7770.mp3",
 )
