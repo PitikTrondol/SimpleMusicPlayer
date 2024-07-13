604f30b2cb0ad166964354f3024e13ee1eacb3f3
Afriandi <emailafriandi@gmail.com>
2024-07-13 15:43:20 +0700

    update (main) : Add data manager to injection

--- app/src/main/java/anridiaf/playground/simplemusicplayer/sources/SourceModule.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/sources/SourceModule.kt
@@ -1,2 +1,14 @@
 package anridiaf.playground.simplemusicplayer.sources
 
+import anridiaf.playground.simplemusicplayer.sources.songdata.SongDataManager
+import anridiaf.playground.simplemusicplayer.sources.songdata.SongDataManagerImpl
+import io.ktor.client.HttpClient
+import org.koin.dsl.module
+
+val dataSourceModule = module {
+    factory<SongDataManager> {
+        SongDataManagerImpl(
+            httpClient = get<HttpClient>()
+        )
+    }
+}
