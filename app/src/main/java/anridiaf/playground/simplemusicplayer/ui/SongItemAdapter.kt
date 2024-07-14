bdf831ac0dc7a0d15926946834e1b489d1c931a3
Afriandi <emailafriandi@gmail.com>
2024-07-14 09:11:06 +0700

    update (main) : Migrate to viewbinding

--- app/src/main/java/anridiaf/playground/simplemusicplayer/ui/SongItemAdapter.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/ui/SongItemAdapter.kt
@@ -4,13 +4,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.annotation.OptIn
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.recyclerview.widget.RecyclerView
 import anridiaf.playground.simplemusicplayer.R
+import anridiaf.playground.simplemusicplayer.databinding.PlaylistItemBinding
 import coil.load
 
 class SongItemAdapter : RecyclerView.Adapter<SongItemAdapter.SongItemView>() {
@@ -53,10 +52,15 @@ class SongItemAdapter : RecyclerView.Adapter<SongItemAdapter.SongItemView>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongItemView {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.playlist_item, parent, false)
-        return SongItemView(view)
+        val binding = PlaylistItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+//        val view = LayoutInflater
+//            .from(parent.context)
+//            .inflate(R.layout.playlist_item, parent, false)
+        return SongItemView(binding)
     }
 
     override fun getItemCount(): Int {
@@ -69,27 +73,21 @@ class SongItemAdapter : RecyclerView.Adapter<SongItemAdapter.SongItemView>() {
 
     @OptIn(UnstableApi::class)
     class SongItemView(
-        private val view: View
-    ) : RecyclerView.ViewHolder(view) {
-
-        private val artwork: ImageView by lazy { view.findViewById(R.id.song_artwork) }
-        private val soundWave: ImageView by lazy { view.findViewById(R.id.song_sound_wave) }
-        private val title: TextView by lazy { view.findViewById(R.id.song_title) }
-        private val artist: TextView by lazy { view.findViewById(R.id.song_artist) }
-        private val album: TextView by lazy { view.findViewById(R.id.song_album) }
+        private val binding: PlaylistItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         fun update(data: AdapterItem) {
             val metadata = data.mediaItem.mediaMetadata
             val artworkUri = metadata.artworkUri?.path.orEmpty()
             if (artworkUri.isNotBlank()) {
-                artwork.load(metadata.artworkUri)
+                binding.songArtwork.load(metadata.artworkUri)
             } else {
-                artwork.load(R.drawable.artwork_placeholder)
+                binding.songArtwork.load(R.drawable.artwork_placeholder)
             }
-            soundWave.visibility = if(data.isPlaying) View.VISIBLE else View.INVISIBLE
-            title.text = metadata.title ?: "Title"
-            artist.text = metadata.artist ?: "Artist"
-            album.text = metadata.albumTitle ?: "Album"
+            binding.songSoundWave.visibility = if(data.isPlaying) View.VISIBLE else View.INVISIBLE
+            binding.songTitle.text = metadata.title ?: "Title"
+            binding.songArtist.text = metadata.artist ?: "Artist"
+            binding.songAlbum.text = metadata.albumTitle ?: "Album"
 //            view.setOnClickListener { onSelectSong(mediaItem) }
         }
     }
