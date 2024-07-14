53a5b39b20cab9e834642f0b92ef57d3f958c562
Afriandi <emailafriandi@gmail.com>
2024-07-14 16:13:33 +0700

    update (main) : Add test for success flow

--- app/src/test/java/anridiaf/playground/simplemusicplayer/presenter/SongManagerViewModelTest.kt
+++ app/src/test/java/anridiaf/playground/simplemusicplayer/presenter/SongManagerViewModelTest.kt
@@ -1,5 +1,6 @@
 package anridiaf.playground.simplemusicplayer.presenter
 
+import android.net.Uri
 import anridiaf.playground.simplemusicplayer.sources.songdata.SongData
 import anridiaf.playground.simplemusicplayer.sources.songdata.SongDataManager
 import anridiaf.playground.simplemusicplayer.utils.CoroutinesTestExtension
@@ -7,9 +8,12 @@ import anridiaf.playground.simplemusicplayer.utils.InstantExecutorExtension
 import anridiaf.playground.simplemusicplayer.utils.ResultWrapper
 import app.cash.turbine.test
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkStatic
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
@@ -28,6 +32,15 @@ class SongManagerViewModelTest {
     val executorExtension = InstantExecutorExtension()
 
     private val songDataManager: SongDataManager = mockk()
+    private fun createViewModel() = SongManagerViewModel(
+        songDataManager = songDataManager,
+        defaultDispatcher = coroutinesExtension.dispatcher,
+    )
+
+    @BeforeEach
+    fun setup() {
+        mockkStatic(Uri::class)
+    }
 
     @Nested
     inner class OnInit {
@@ -82,16 +95,18 @@ class SongManagerViewModelTest {
         fun `WHEN songDataManager return list THEN uiStateFlow UiState_Success_data SHOULD NOT BE empty`() =
             runTest {
                 // Arrange
+                val expectedSource = "https://cdn.pixabay.com/"
                 val expectedData = listOf(
                     SongData(
-                        thumbnail= "thumbnail",
-                        sources= "sources",
-                        artist= "artist",
-                        title= "title",
+                        thumbnail = "thumbnail",
+                        sources = expectedSource,
+                        artist = "artist",
+                        title = "title",
                         album = "album"
                     )
                 )
 
+                every { Uri.parse(any()) } returns mockk<Uri>()
                 coEvery { songDataManager.getList() } returns ResultWrapper.Success(expectedData)
 
                 // Act
@@ -102,16 +117,45 @@ class SongManagerViewModelTest {
 
                 // Assert
                 sut.uiStateFlow.test {
-//                    assertTrue(awaitItem() is UiState.Init)
-//                    assertTrue(awaitItem() is UiState.Loading)
-//                    val actual = awaitItem()
-//                    assertTrue(actual is UiState.Success)
-//                    assertEquals(mapper.from(expectedData), actual.data)
+                    assertTrue(awaitItem() is UiState.Init)
+                    assertTrue(awaitItem() is UiState.Loading)
 
-                    println(awaitItem())
-                    println(awaitItem())
-                    println(awaitItem())
+                    val actual = awaitItem()
+                    assertTrue(actual is UiState.Success)
+                    assertTrue(actual.data.isNotEmpty())
+                    val expectedUri = Uri.parse(expectedData[0].sources)
+                    val actualUri = actual.data[0].mediaMetadata.artworkUri
+                    assertEquals(expectedUri, actualUri)
                 }
             }
     }
+
+    @Nested
+    inner class Filter {
+        @Test
+        fun `WHEN query is blank THEN uiStateFlow data SHOULD reset`() = runTest {
+            // Arrange
+
+            // Act
+            val sut = createViewModel()
+
+            // Assert
+        }
+    }
+
+    private val listMediaItem = listOf(
+        defaultSongData(artis = "Hafnarfjordur"),
+        defaultSongData(artis = "Tarno"),
+        defaultSongData(title = "Break it down"),
+        defaultSongData(artis = "Megumi"),
+        defaultSongData(title = "Random"),
+    )
+
+    private fun defaultSongData(artis: String = "", title: String = "") = SongData(
+        thumbnail = "",
+        sources = "",
+        artist = title,
+        title = title,
+        album = ""
+    )
 }
