2fe7305cd81454318c5a7e4373f8758706dbfb5f
Afriandi <emailafriandi@gmail.com>
2024-07-14 16:11:44 +0700

    update (main) : Refactor with Ctrl + Alt + L

--- app/src/main/java/anridiaf/playground/simplemusicplayer/sources/songdata/SongDataManagerImpl.kt
+++ app/src/main/java/anridiaf/playground/simplemusicplayer/sources/songdata/SongDataManagerImpl.kt
@@ -1,6 +1,5 @@
 package anridiaf.playground.simplemusicplayer.sources.songdata
 
-import android.util.Log
 import anridiaf.playground.simplemusicplayer.utils.ResultWrapper
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -10,27 +9,28 @@ import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 
 private const val GIST_URL = "https://api.github.com/gists/03c76ce55e73f088372e146055fc9d7e"
+
 class SongDataManagerImpl(
     private val httpClient: HttpClient,
     private val dispatcher: CoroutineDispatcher
-): SongDataManager {
-    override suspend fun getList(): ResultWrapper<List<SongData>> = withContext(dispatcher){
+) : SongDataManager {
+    override suspend fun getList(): ResultWrapper<List<SongData>> = withContext(dispatcher) {
         try {
             val body = httpClient.get(GIST_URL).body<GistResponse>()
             val jsonString = body.files?.playlist?.content.orEmpty()
             val resultList = Json.decodeFromString<SongDataList>(jsonString)
-            val result = resultList.songData?.map { response->
+            val result = resultList.songData?.map { response ->
                 SongData(
-                    thumbnail= response?.thumbnail.orEmpty(),
-                    sources= response?.sources.orEmpty(),
-                    artist= response?.artist.orEmpty(),
-                    title= response?.title.orEmpty(),
+                    thumbnail = response?.thumbnail.orEmpty(),
+                    sources = response?.sources.orEmpty(),
+                    artist = response?.artist.orEmpty(),
+                    title = response?.title.orEmpty(),
                     album = response?.album.orEmpty()
                 )
             } ?: listOf()
 
             ResultWrapper.Success(result)
-        }catch (e: Exception){
+        } catch (e: Exception) {
             ResultWrapper.Error(statusResponse = e.message ?: "Exception on get song data")
         }
     }
