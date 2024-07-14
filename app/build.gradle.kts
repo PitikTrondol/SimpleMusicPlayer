8a742bd8a4fc52b7f5b46acb63cb7eb567110de8
Afriandi <emailafriandi@gmail.com>
2024-07-14 20:35:44 +0700

    merge (main) : Add release note to actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "anridiaf.playground.simplemusicplayer"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
